7dcda698ea0e5bd2e185987be4a4fe7e4d7af770
Efox6179 <95189308+Efox6179@users.noreply.github.com>
2022-01-16 15:54:24 -0600

    questions added

---
+++ assets/js/questions.js
@@ -0,0 +1,116 @@
+//these are the questions that will be referenced in the quiz with the correct answers and all of the choices 
+
+
+let questions = [
+    {
+    numb: 1,
+    question: "What is the name of this car part?",
+    answer: "Alternator",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the name of this car part?",
+    answer: "Clutch Disc",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the name of this car part?",
+    answer: "Connecting Rod",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the name of this car part?",
+    answer: "Fuel Injector",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the name of this car part?",
+    answer: "Head Gasket",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+},
+{
+    numb: 6,
+    question: "What is the name of this car part?",
+    answer: "Spark Plug",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+    {
+    numb: 7,
+    question: "What is the name of this car part?",
+    answer: "Sway Bar",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+    {
+    numb: 8,
+    question: "What is the name of this car part?",
+    answer: "Turbo Charger",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+    {
+    numb: 9,
+    question: "What is the name of this car part?",
+    answer: "Valves",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+    {
+    numb: 10,
+    question: "What is the name of this car part?",
+    answer: "Water Pump",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+},
+
+]
